8c20cc5439c955954f3330b10e3dabf0f5dee583
Molly Setzer <molly.setzer@gmail.com>
2015-10-07 21:01:58 -0400

    added server

---
+++ portfolio/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// app.get('/', function(req, res) {
+// 	res.send('Hello World');
+// });
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.listen(8080);
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example appp is listening at http', host, port);
+});
